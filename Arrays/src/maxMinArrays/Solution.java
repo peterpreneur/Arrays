f2cb747631616d45e2c642950632ece548eef3f3
pas8 <peterpreneur@gmail.com>
2022-02-08 23:07:39 -0600

    intro

---
+++ Arrays/src/maxMinArrays/Solution.java
@@ -0,0 +1,10 @@
+package maxMinArrays;
+
+public class Solution {
+
+	public static void main(String[] args) {
+	  //public static int[] solve (int[] arr){  		
+		// TODO Auto-generated method stub
+
+	}
+}
