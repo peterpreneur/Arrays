bb33e2f5ade27da65294fc25fed9cdc2ef58c1ab
pas8 <peterpreneur@gmail.com>
2022-02-09 18:59:02 -0600

    intro

--- Arrays/src/addValueinArray/AddValueinArray.java
+++ Arrays/src/addValueinArray/AddValueinArray.java
@@ -1,7 +1,19 @@
 package addValueinArray;
 
+import java.util.Arrays;
+
+//You have to write a method, that gets two parameter:
+//1. An array of arrays with int-numbers
+//2. The shifting value
+
 public class AddValueinArray {
 	public static int[] addingShifted(int[][] arrayOfArrays, int shift) {
-		return arrayOfArrays[0];
+	int arrLen = arrayOfArrays.length;
+	int i; int j;
+	
+	for(i=0, j=0; i < arrLen; i++, j++) {
+		
+		
+		//return arrayOfArrays[0];
 	}
 }
