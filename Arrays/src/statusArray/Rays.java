5d83cfa1761d05c9e4c04e80e822850d009fd574
pas8 <peterpreneur@gmail.com>
2022-02-10 20:08:42 -0600

    intro

--- Arrays/src/statusArray/Rays.java
+++ Arrays/src/statusArray/Rays.java
@@ -1,7 +1,12 @@
 package statusArray;
 
-public class Rays{
-	  public static int[] status(int[] nums){
-	    return new int[]{}; //Let's Crank Some Code!
-	  }
+import java.util.stream.IntStream;
+
+public class Rays {
+	// public static int[] status(int[] nums){
+	public static void main(String args[]) {
+		int[] arr1 = { 5, 4, 3, 2, 1 };
+		
+		IntStream.of(arr1);
 	}
+}
