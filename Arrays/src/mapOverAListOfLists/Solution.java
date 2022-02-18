bed868992d639f935ff34fe635d7d7ebc7cc53d1
pas8 <peterpreneur@gmail.com>
2022-02-18 11:12:31 -0600

    ListofLIsts

--- Arrays/src/mapOverAListOfLists/Solution.java
+++ Arrays/src/mapOverAListOfLists/Solution.java
@@ -4,7 +4,8 @@ import java.util.function.Function;
 
 public class Solution{
     public static <T,R> R[][] gridMap(Function<T,R> fn, T[][] list){
-        //TODO: write the method
-        return null;
+    	
+    	
+    	return null;
     }
 }
