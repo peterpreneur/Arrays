f68f8f6252be6ec8869501ab78bb422cb0915771
pas8 <peterpreneur@gmail.com>
2022-02-19 02:33:44 -0600

    rotate2D

--- Arrays/src/rotate2D/Rotate2D.java
+++ Arrays/src/rotate2D/Rotate2D.java
@@ -8,13 +8,13 @@ public class Rotate2D {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[][] input = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
-		
+
 		int[][] tempArr1 = rotate(input);
-		
+
+		System.out.println(Arrays.deepToString(tempArr1));
 	}
 
 	public static int[][] rotate(int[][] matrix) {
-
 		int numOfIntArrays = matrix.length;
 		int lenOfIntArray = matrix[0].length;
 
